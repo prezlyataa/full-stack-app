51c4e4d1e2eb84c77a7994e192c041ef106fbc59
Yurii Prezliata <yurii.prezliata@synapse.com>
2021-06-10 20:10:45 +0300

    changed name for schema

--- user.js
+++ user.js
@@ -1,23 +1,8 @@
 const mongoose = require("mongoose");
 
-const user = new mongoose.Schema({
+const User = new mongoose.Schema({
     username: String,
     password: String,
 });
 
-module.exports = mongoose.model("User", user);
-
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-
-// const userSchema = new Schema(
-//   {
-//     username: { type: String, required: true },
-//     password: { type: Number, required: true },
-//   },
-//   { collection: "users" }
-// );
-
-// const User = mongoose.model("User", userSchema);
-
-// module.exports = User;
+module.exports = mongoose.model("User", User);
